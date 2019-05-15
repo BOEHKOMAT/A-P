400033b4db9981b134e8a8a1ffc7717e07d3b1b2
BOEHKOMAT <44070912+BOEHKOMAT@users.noreply.github.com>
2019-05-15 11:10:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int a = 1, b = 3, c = 5 , d = 7, e = 9;
+    cout << "+--------------------+" << endl;
+    cout << "| " << a*a << " | " << b*b << " | " << c*c << " |" << d*d << " | " << e*e << "|" << endl;
+    cout << "+--------------------+" << endl;
+    return 0;
+}
